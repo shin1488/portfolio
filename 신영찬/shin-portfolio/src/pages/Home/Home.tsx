65b10635787bc47f7ed70ca643451b227b1f12d0
shin1488 <syc1488@naver.com>
2026-03-03 07:03:00 +0900

    변경 : 홈 페이지 포스트 누락 부분 수정

--- 신영찬/shin-portfolio/src/pages/Home/Home.tsx
+++ 신영찬/shin-portfolio/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import BottomNavBar from "../../components/BottomNavBar/BottomNavBar";
 import CareersContainer from "../../containers/CareersContainer/CareersContainer";
 import IntroduceContainer from "../../containers/IntroduceContainer/IntroduceContainer";
+import PostContainer from "../../containers/PostCardContainer/PostCardContainer";
 import ProjectContainer from "../../containers/ProjectContainer/ProjectContainer";
 import SkillsContainer from "../../containers/SkillsContainer/SkillsContainer";
 import styles from './Home.module.css';
@@ -12,6 +13,7 @@ const Home = () => {
             <SkillsContainer />
             <CareersContainer />
             <ProjectContainer limit={3} />
+            <PostContainer />
 
             <BottomNavBar />
         </div>
