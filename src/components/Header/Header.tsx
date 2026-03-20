ea9c636b7757b556f3b935878b2d0363b501b7d4
shin1488 <syc1488@naver.com>
2026-03-20 14:51:36 +0900

    변경 : 프로젝트 구조 변경, 미사용 파일 삭제

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.title_section}>
+        <p className={styles.title}>신영찬의 포트폴리오</p>
+      </Link>
+      <div className={styles.nav_section}>
+        <Link to="/" className={styles.nav_item}>
+          Home
+        </Link>
+        <Link to="/projects" className={styles.nav_item}>
+          Projects
+        </Link>
+        <Link to="/posts" className={styles.nav_item}>
+          Blog
+        </Link>
+        <Link to="/guestbook" className={styles.nav_item}>
+          GuestBook
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
